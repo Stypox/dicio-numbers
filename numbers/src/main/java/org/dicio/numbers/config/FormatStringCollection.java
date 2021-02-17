d0b045c1bb537d95b7cdd297aec931d03dbb169b
Stypox <stypox@pm.me>
2021-02-17 12:30:51 +0100

    Add FormatStringCollection class
    It chooses the best format string to use to format a number based on the provided patterns

---
+++ numbers/src/main/java/org/dicio/numbers/config/FormatStringCollection.java
@@ -0,0 +1,41 @@
+package org.dicio.numbers.config;
+
+import com.grack.nanojson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class FormatStringCollection {
+
+    private static class PatternFormatPair {
+        final Pattern pattern;
+        final FormatString formatString;
+
+        private PatternFormatPair(final JsonObject jsonObject) {
+            pattern = Pattern.compile(jsonObject.getString("match"));
+            formatString = new FormatString(jsonObject.getString("format"));
+        }
+    }
+
+    private final FormatString defaultFormat;
+    private final List<PatternFormatPair> patternFormats;
+
+    public FormatStringCollection(final JsonObject jsonObject) {
+        defaultFormat = new FormatString(jsonObject.getString("default"));
+        patternFormats = new ArrayList<>();
+        for (int i = 1; jsonObject.has(String.valueOf(i)); ++i) {
+            patternFormats.add(new PatternFormatPair(jsonObject.getObject(String.valueOf(i))));
+        }
+    }
+
+    public FormatString getMostSuitableFormatString(final int number) {
+        final String numberString = String.valueOf(number);
+        for (final PatternFormatPair patternFormat : patternFormats) {
+            if (patternFormat.pattern.matcher(numberString).matches()) {
+                return patternFormat.formatString;
+            }
+        }
+        return defaultFormat;
+    }
+}
---
+++ numbers/src/test/java/org/dicio/numbers/config/FormatStringCollectionTest.java
@@ -0,0 +1,81 @@
+package org.dicio.numbers.config;
+
+import com.grack.nanojson.JsonParser;
+import com.grack.nanojson.JsonParserException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class FormatStringCollectionTest {
+
+    private static final String defaultJson = "{" +
+            "\"1\": {\"match\": \"^\\\\d$\", \"format\": \"{x}\"}," +
+            "\"2\": {\"match\": \"^1\\\\d$\", \"format\": \"{xx}\"}," +
+            "\"3\": {\"match\": \"^\\\\d0$\", \"format\": \"{x0}\"}," +
+            "\"4\": {\"match\": \"^[2-9]\\\\d$\", \"format\": \"{x0} {x}\"}," +
+            "\"default\": \"{number}\"" +
+            "}";
+
+    private static final Map<String, String> substitutionTable = new HashMap<String, String>() {{
+        put("x", "five");
+        put("xx", "65");
+        put("x0", "sixty");
+        put("number", "1965");
+    }};
+
+    private static FormatStringCollection fsc;
+
+    private static void assertFormattedTo(final String expected, final int number) {
+        assertEquals(number + " is not formatted correctly", expected, fsc.getMostSuitableFormatString(number).format(substitutionTable));
+    }
+
+
+    @BeforeClass
+    public static void setup() throws JsonParserException {
+        fsc = new FormatStringCollection(JsonParser.object().from(defaultJson));
+    }
+
+    @Test
+    public void singleDigit() {
+        for (int i = 0; i < 10; ++i) {
+            assertFormattedTo("five", i);
+        }
+    }
+
+    @Test
+    public void twoDigitLessThanTwenty() {
+        for (int i = 10; i < 20; ++i) {
+            assertFormattedTo("65", i);
+        }
+    }
+
+    @Test
+    public void twoDigitMoreThanTwenty() {
+        for (int i = 20; i < 100; ++i) {
+            if (i % 10 == 0) {
+                assertFormattedTo("sixty", i);
+            } else {
+                assertFormattedTo("sixty five", i);
+            }
+        }
+    }
+
+    @Test
+    public void otherDigitCount() {
+        for (final int i : new int[] {100, 4387, 192, 448, 37482, Integer.MAX_VALUE}) {
+            assertFormattedTo("1965", i);
+        }
+    }
+
+    @Test
+    public void negative() {
+        for (final int i : new int[] {-1, -8, -32, -123, -53798, Integer.MIN_VALUE}) {
+            assertFormattedTo("1965", i);
+        }
+    }
+}
