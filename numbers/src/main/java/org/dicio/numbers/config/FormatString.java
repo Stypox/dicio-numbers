5a5316e197bab81ed3399d15e1e7e594cd0d579c
Stypox <stypox@pm.me>
2021-02-17 12:29:17 +0100

    Add FormatString to parse and format strings with {key}

---
+++ numbers/src/main/java/org/dicio/numbers/config/FormatString.java
@@ -0,0 +1,76 @@
+package org.dicio.numbers.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class FormatString {
+
+    private interface Part {
+        void format(Map<String, String> substitution, StringBuilder stringBuilder);
+    }
+
+    private static class StringPart implements Part {
+        private final String value;
+        StringPart(final String value) {
+            this.value = value;
+        }
+
+        @Override
+        public void format(final Map<String, String> substitution,
+                           final StringBuilder stringBuilder) {
+            stringBuilder.append(value);
+        }
+    }
+
+    private static class FormatPart implements Part {
+        private final String key;
+        FormatPart(final String key) {
+            this.key = key;
+        }
+
+        @Override
+        public void format(final Map<String, String> substitutionTable,
+                           final StringBuilder stringBuilder) {
+            if (!substitutionTable.containsKey(key)){
+                throw new IllegalArgumentException("Missing key " + key);
+            }
+            stringBuilder.append(substitutionTable.get(this.key));
+        }
+    }
+
+
+    final List<Part> parts;
+
+    public FormatString(final String stringToParse) {
+        parts = new ArrayList<>();
+        int prevIndex = 0;
+        while (prevIndex < stringToParse.length()) {
+            final int beginIndex = stringToParse.indexOf('{', prevIndex);
+            if (beginIndex < 0) {
+                parts.add(new StringPart(stringToParse.substring(prevIndex)));
+                break;
+            }
+
+            final int endIndex = stringToParse.indexOf('}', beginIndex + 1);
+            if (endIndex < 0) {
+                parts.add(new StringPart(stringToParse.substring(prevIndex)));
+                break;
+            }
+
+            if (beginIndex != prevIndex) {
+                parts.add(new StringPart(stringToParse.substring(prevIndex, beginIndex)));
+            }
+            parts.add(new FormatPart(stringToParse.substring(beginIndex + 1, endIndex)));
+            prevIndex = endIndex + 1;
+        }
+    }
+
+    public String format(final Map<String, String> substitutionTable) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (final Part part : parts) {
+            part.format(substitutionTable, stringBuilder);
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ numbers/src/test/java/org/dicio/numbers/config/FormatStringTest.java
@@ -0,0 +1,68 @@
+package org.dicio.numbers.config;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class FormatStringTest {
+
+    private static final Map<String, String> substitutionTable = new HashMap<String, String>() {{
+        put("a", "A");
+        put("bbb", "B1");
+        put("empty", "");
+        put("space", " ");
+        put("never used", "unreachable");
+    }};
+
+    private static void assertFormat(final String expected, final String formatString) {
+        assertEquals(expected, new FormatString(formatString).format(substitutionTable));
+    }
+
+
+    @Test
+    public void noFormatParts() {
+        assertFormat("hello", "hello");
+        assertFormat("    ", "    ");
+        assertFormat(" { {{ ", " { {{ ");
+        assertFormat("} {k127{", "} {k127{");
+    }
+
+    @Test
+    public void onlyOneFormatPart() {
+        assertFormat("A", "{a}");
+        assertFormat("B1", "{bbb}");
+        assertFormat("", "{empty}");
+        assertFormat(" ", "{space}");
+    }
+
+    @Test
+    public void onlyFormatParts() {
+        assertFormat("B1A ", "{bbb}{a}{space}");
+        assertFormat("AA AB1A", "{a}{empty}{a}{space}{empty}{a}{bbb}{empty}{a}{empty}");
+        assertFormat("    ", "{space}{space}{empty}{space}{space}");
+        assertFormat("", "{empty}{empty}{empty}");
+    }
+
+    @Test
+    public void mixedParts() {
+        assertFormat("A, B1 A", "{a}, {bbb}{space}{empty}{a}");
+        assertFormat("    ciAoB1 ", "  {space}{space}ci{a}o{bbb} ");
+        assertFormat("  }B1} A{a", "{space} }{bbb}} {a}{a");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fails() {
+        new FormatString(" {a} {empty}{bbba}{space}").format(substitutionTable);
+    }
+
+    @Test
+    public void emptyFormatString() {
+        final FormatString fs = new FormatString("");
+        assertEquals("", fs.format(Collections.emptyMap()));
+        assertEquals("", fs.format(substitutionTable));
+    }
+}
