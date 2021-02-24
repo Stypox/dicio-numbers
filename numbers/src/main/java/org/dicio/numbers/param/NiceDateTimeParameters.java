f1461dc58af16553b0d8f1b06eddfa2dcf29d5e1
Stypox <stypox@pm.me>
2021-02-24 22:04:55 +0100

    Add convenience method for niceDateTime to NumberParserFormatter

---
+++ numbers/src/main/java/org/dicio/numbers/param/NiceDateTimeParameters.java
@@ -0,0 +1,50 @@
+package org.dicio.numbers.param;
+
+import org.dicio.numbers.NumberFormatter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class NiceDateTimeParameters {
+
+    private final NumberFormatter numberFormatter;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    // default values
+    private LocalDate now = null;
+    private boolean use24Hour = false;
+    private boolean showAmPm = false;
+
+    public NiceDateTimeParameters(final NumberFormatter numberFormatter,
+                                  final LocalDateTime dateTime) {
+        this.numberFormatter = numberFormatter;
+        this.date = dateTime.toLocalDate();
+        this.time = dateTime.toLocalTime();
+    }
+
+    /**
+     *
+     * @param now nullable
+     * @return
+     */
+    public NiceDateTimeParameters now(final LocalDate now) {
+        this.now = now;
+        return this;
+    }
+
+    public NiceDateTimeParameters use24Hour(final boolean use24Hour) {
+        this.use24Hour = use24Hour;
+        return this;
+    }
+
+    public NiceDateTimeParameters showAmPm(final boolean showAmPm) {
+        this.showAmPm = showAmPm;
+        return this;
+    }
+
+    public String get() {
+        return numberFormatter.niceDateTime(date, now, time, use24Hour, showAmPm);
+    }
+}
