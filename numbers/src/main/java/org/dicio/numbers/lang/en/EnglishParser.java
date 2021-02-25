f57bf6fc51f1f7a49343b5abce2439accfde2299
Stypox <stypox@pm.me>
2021-02-25 09:59:38 +0100

    Add EnglishParser with no implemented functions

---
+++ numbers/src/main/java/org/dicio/numbers/lang/en/EnglishParser.java
@@ -0,0 +1,56 @@
+package org.dicio.numbers.lang.en;
+
+import org.dicio.numbers.NumberParser;
+import org.dicio.numbers.param.Gender;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+public class EnglishParser extends NumberParser {
+    @Override
+    public double extractNumber(final String text,
+                                final boolean shortScale,
+                                final boolean ordinals) {
+        return 0;
+    }
+
+    @Override
+    public List<Double> extractNumbers(final String text,
+                                       final boolean shortScale,
+                                       final boolean ordinals) {
+        return null;
+    }
+
+    @Override
+    public long extractDuration(final String text) {
+        return 0;
+    }
+
+    @Override
+    public LocalDateTime extractDateTime(final String text,
+                                         final boolean anchorDate,
+                                         final LocalTime defaultTime) {
+        return null;
+    }
+
+    @Override
+    public String normalize(final String text, final boolean removeArticles) {
+        return null;
+    }
+
+    @Override
+    public Gender getGender(final String text, final String context) {
+        return Gender.UNKNOWN;
+    }
+
+    @Override
+    public boolean isFractional(final String text, final boolean shortScale, final boolean spoken) {
+        return false;
+    }
+
+    @Override
+    public boolean isOrdinal(final String text, final boolean shortScale, final boolean spoken) {
+        return false;
+    }
+}
