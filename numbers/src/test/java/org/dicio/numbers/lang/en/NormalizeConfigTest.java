943a8946b523332614037173a978195444e4a423
Stypox <stypox@pm.me>
2021-02-28 10:42:13 +0100

    Add tests for NormalizeConfig

---
+++ numbers/src/test/java/org/dicio/numbers/lang/en/NormalizeConfigTest.java
@@ -0,0 +1,10 @@
+package org.dicio.numbers.lang.en;
+
+import org.dicio.numbers.parser.normalize.NormalizeConfigTestBase;
+
+public class NormalizeConfigTest extends NormalizeConfigTestBase {
+    @Override
+    public String configFolder() {
+        return "config/en-us";
+    }
+}
---
+++ numbers/src/test/java/org/dicio/numbers/parser/normalize/NormalizeConfigDefaultTest.java
@@ -0,0 +1,8 @@
+package org.dicio.numbers.parser.normalize;
+
+public class NormalizeConfigDefaultTest extends NormalizeConfigTestBase {
+    @Override
+    public String configFolder() {
+        return null;
+    }
+}
