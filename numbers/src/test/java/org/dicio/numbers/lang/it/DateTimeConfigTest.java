830fbdb9e393af8c12847d58fe2d13498af23f85
Stypox <stypox@pm.me>
2021-07-30 18:21:50 +0200

    [it-it] Add DateTimeConfigTest, it fails due to no trimming

---
+++ numbers/src/test/java/org/dicio/numbers/lang/it/DateTimeConfigTest.java
@@ -0,0 +1,10 @@
+package org.dicio.numbers.lang.it;
+
+import org.dicio.numbers.formatter.datetime.DateTimeConfigTestBase;
+
+public class DateTimeConfigTest extends DateTimeConfigTestBase {
+    @Override
+    public String configFolder() {
+        return "config/it-it";
+    }
+}
