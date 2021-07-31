69d861535fbe98c46a41452272940c3141b3e44e
Stypox <stypox@pm.me>
2021-07-31 12:00:58 +0200

    [it-it] Add DateTimeTest

---
+++ numbers/src/test/java/org/dicio/numbers/lang/it/DateTimeTest.java
@@ -0,0 +1,46 @@
+package org.dicio.numbers.lang.it;
+
+import org.dicio.numbers.formatter.NumberFormatter;
+import org.dicio.numbers.formatter.datetime.DateTimeTestBase;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateTimeTest extends DateTimeTestBase {
+
+    @Override
+    public String configFolder() {
+        return "config/it-it";
+    }
+
+    @Override
+    public NumberFormatter buildNumberFormatter() {
+        return new ItalianFormatter();
+    }
+
+    @Test
+    public void testNiceDate() {
+        // just check that the NumberParserFormatter functions do their job
+        assertEquals("mercoledì, ventotto aprile, duemila ventuno",
+                pf.niceDate(LocalDate.of(2021, 4, 28)).get());
+        assertEquals("domenica, tredici agosto",
+                pf.niceDate(LocalDate.of(-84, 8, 13)).now(LocalDate.of(-84, 8, 23)).get());
+    }
+
+    @Test
+    public void testNiceYear() {
+        // just check that the NumberParserFormatter functions do their job
+        assertEquals("mille novecento ottantaquattro", pf.niceYear(LocalDate.of(1984, 4, 28)).get());
+        assertEquals("ottocento dieci a.C.", pf.niceYear(LocalDate.of(-810, 8, 13)).get());
+    }
+
+    @Test
+    public void testNiceDateTime() {
+        // just check that the NumberParserFormatter functions do their job
+        assertEquals("mercoledì, dodici settembre, mille settecento sessantaquattro alle mezzogiorno in punto", pf.niceDateTime(LocalDateTime.of(1764, 9, 12, 12, 0)).get());
+        assertEquals("giovedì, tre novembre, trecento ventotto a.C. alle cinque e zero sette", pf.niceDateTime(LocalDateTime.of(-328, 11, 3, 5, 7)).get());
+    }
+}
