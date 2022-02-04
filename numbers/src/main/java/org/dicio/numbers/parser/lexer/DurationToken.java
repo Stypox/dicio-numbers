bdea0494c1ed21355f923a5bcb62719aa01b4780
Stypox <stypox@pm.me>
2022-02-04 23:24:24 +0100

    Add DurationToken with duration multiplication

---
+++ numbers/src/main/java/org/dicio/numbers/parser/lexer/DurationToken.java
@@ -0,0 +1,44 @@
+package org.dicio.numbers.parser.lexer;
+
+import org.dicio.numbers.util.Number;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.time.Duration;
+
+public class DurationToken extends Token {
+    private final Duration durationMultiplier;
+    private final boolean restrictedAfterNumber;
+
+    DurationToken(final String value,
+                  final String spacesFollowing,
+                  final Duration durationMultiplier,
+                  final boolean restrictedAfterNumber) {
+        super(value, spacesFollowing);
+        this.durationMultiplier = durationMultiplier;
+        this.restrictedAfterNumber = restrictedAfterNumber;
+    }
+
+    public Duration getDurationMultiplier() {
+        return durationMultiplier;
+    }
+
+    public boolean isRestrictedAfterNumber() {
+        return restrictedAfterNumber;
+    }
+
+    public Duration getDurationMultipliedBy(final Number number) {
+        if (number.isInteger()) {
+            return durationMultiplier.multipliedBy(number.integerValue());
+        } else {
+            final BigDecimal allNanos = BigDecimal.valueOf(durationMultiplier.getNano())
+                    .add(BigDecimal.valueOf(durationMultiplier.getSeconds(), -9))
+                    .multiply(BigDecimal.valueOf(number.decimalValue()));
+            final long seconds = allNanos.multiply(BigDecimal.valueOf(1, 9)).longValue();
+            final long nanos = allNanos.subtract(BigDecimal.valueOf(seconds, -9)).longValue();
+
+            return Duration.ofSeconds(seconds, nanos);
+        }
+    }
+}
