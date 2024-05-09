62d10514218441a7619c909fd2940559606b0b4f
Stypox <stypox@pm.me>
2024-05-09 09:15:38 +0200

    Rename NumberParserFormatter to ParserFormatter

--- numbers/src/main/java/org/dicio/numbers/ParserFormatter.java
+++ numbers/src/main/java/org/dicio/numbers/ParserFormatter.java
@@ -8,5 +8,5 @@ package org.dicio.numbers;
  * @see <a href="https://github.com/Stypox/dicio-numbers/tree/dummy#readme">
  *     the README of the dummy implementation of dicio-numbers</a>
  */
-public interface NumberParserFormatter {
+public interface ParserFormatter {
 }
