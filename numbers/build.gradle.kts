1cf2e36a6e83747636f43abca152b74ae690a4ae
Stypox <stypox@pm.me>
2024-05-27 16:33:33 +0200

    Revert "Add GitHub action to build artifact"
    This reverts commit d54de33bb21a5b8dfa19bc58d51b3a8166103648.

--- numbers/build.gradle.kts
+++ numbers/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
     id("java-library")
-    id("maven-publish")
 }
 
 kotlin {
@@ -11,32 +10,4 @@ kotlin {
 dependencies {
     implementation(libs.nanojson)
     testImplementation(libs.junit)
-}
-
-val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.convention("sources");
-    archiveClassifier.set("sources");
-    from(sourceSets.main.get().allSource)
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Stypox/dicio-numbers")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        register("mavenJava", MavenPublication::class) {
-            groupId = "org.dicio"
-            artifactId = "numbers"
-            version = System.getenv("COMMIT_SHA")
-            from(components["java"])
-            artifact(sourcesJar.get())
-        }
-    }
 }
