fd7368769cef877edba68bd1d67af61b676fcb7c
Stypox <stypox@pm.me>
2026-02-24 17:45:04 +0100

    Also test positionInOriginalString for Tokenizer

--- numbers/src/test/java/org/dicio/numbers/parser/lexer/TokenizerTest.java
+++ numbers/src/test/java/org/dicio/numbers/parser/lexer/TokenizerTest.java
@@ -20,29 +20,33 @@ public class TokenizerTest extends WithTokenizerTestBase {
 
     private static void assertBaseToken(final Token token,
                                         final String value,
-                                        final String spacesFollowing) {
+                                        final String spacesFollowing,
+                                        final int positionInOriginalString) {
         assertEquals(value, token.value);
         assertEquals(spacesFollowing, token.spacesFollowing);
+        assertEquals(positionInOriginalString, token.positionInOriginalString);
         assertTrue(token.isValue(value));
         assertFalse(token.isValue(spacesFollowing));
     }
 
     private static void assertToken(final Token token,
                                     final String value,
-                                    final String spacesFollowing) {
+                                    final String spacesFollowing,
+                                    final int positionInOriginalString) {
         assertEquals("token is not an unmatched Token: "
-                + token.getClass().getSimpleName(), token.getClass(), Token.class);
-        assertBaseToken(token, value, spacesFollowing);
+                + token.getClass().getSimpleName(), Token.class, token.getClass());
+        assertBaseToken(token, value, spacesFollowing, positionInOriginalString);
     }
 
     private static void assertToken(final Token token,
                                     final String value,
                                     final String spacesFollowing,
+                                    final int positionInOriginalString,
                                     final String[] categories,
                                     final String[] notCategories) {
         assertTrue("token is not a MatchedToken: " + token.getClass().getSimpleName(),
                 token instanceof MatchedToken);
-        assertBaseToken(token, value, spacesFollowing);
+        assertBaseToken(token, value, spacesFollowing, positionInOriginalString);
         for (final String category : categories) {
             assertTrue("Missing category " + category, token.hasCategory(category));
         }
@@ -54,12 +58,13 @@ public class TokenizerTest extends WithTokenizerTestBase {
     private static void assertToken(final Token token,
                                     final String value,
                                     final String spacesFollowing,
+                                    final int positionInOriginalString,
                                     final String[] categories,
                                     final String[] notCategories,
                                     final Number number) {
         assertTrue("token is not a NumberToken: " + token.getClass().getSimpleName(),
                 token instanceof NumberToken);
-        assertToken(token, value, spacesFollowing, categories, notCategories);
+        assertToken(token, value, spacesFollowing, positionInOriginalString, categories, notCategories);
         assertEquals(number, token.getNumber());
     }
 
@@ -71,36 +76,36 @@ public class TokenizerTest extends WithTokenizerTestBase {
     @Test
     public void everything() {
         final List<Token> tokens = tokenizer.tokenize("   Hello, twenTy second; plus 1928point \"half\" a-million");
-        assertToken(tokens.get(0),  "",        "   ");
-        assertToken(tokens.get(1),  "Hello",   "");
-        assertToken(tokens.get(2),  ",",       " ",   cat("ignore", "thousand_separator"), cat("point"));
-        assertToken(tokens.get(3),  "twenTy",  " ",   cat("number", "tens"),               cat("ordinal"),    new Number(20));
-        assertToken(tokens.get(4),  "second",  "; ",  cat("number", "ordinal", "digit"),   cat("tens"),       new Number(2));
-        assertToken(tokens.get(5),  "plus",    " ",   cat("sign", "positive"),             cat("number"));
-        assertToken(tokens.get(6),  "1928",    "",    cat("raw", "number"),                cat("digit"),      new Number(1928));
-        assertToken(tokens.get(7),  "point",   " \"", cat("point"),                        cat("ignore"));
-        assertToken(tokens.get(8),  "half",    "\" ", cat("number", "suffix_multiplier"),  cat("multiplier"), new Number(0.5));
-        assertToken(tokens.get(9),  "a",       "",    cat("ignore"),                       cat("sign"));
-        assertToken(tokens.get(10), "-",       "",    cat("ignore", "sign", "negative"),   cat("positive"));
-        assertToken(tokens.get(11), "million", "",    cat("number", "multiplier"),         cat("tens"),       new Number(1000000));
+        assertToken(tokens.get(0),  "",        "   ", 0);
+        assertToken(tokens.get(1),  "Hello",   "",    3);
+        assertToken(tokens.get(2),  ",",       " ",   8,  cat("ignore", "thousand_separator"), cat("point"));
+        assertToken(tokens.get(3),  "twenTy",  " ",   10, cat("number", "tens"),               cat("ordinal"),    new Number(20));
+        assertToken(tokens.get(4),  "second",  "; ",  17, cat("number", "ordinal", "digit"),   cat("tens"),       new Number(2));
+        assertToken(tokens.get(5),  "plus",    " ",   25, cat("sign", "positive"),             cat("number"));
+        assertToken(tokens.get(6),  "1928",    "",    30, cat("raw", "number"),                cat("digit"),      new Number(1928));
+        assertToken(tokens.get(7),  "point",   " \"", 34, cat("point"),                        cat("ignore"));
+        assertToken(tokens.get(8),  "half",    "\" ", 41, cat("number", "suffix_multiplier"),  cat("multiplier"), new Number(0.5));
+        assertToken(tokens.get(9),  "a",       "",    47, cat("ignore"),                       cat("sign"));
+        assertToken(tokens.get(10), "-",       "",    48, cat("ignore", "sign", "negative"),   cat("positive"));
+        assertToken(tokens.get(11), "million", "",    49, cat("number", "multiplier"),         cat("tens"),       new Number(1000000));
     }
 
     @Test
     public void plurals() {
         final List<Token> tokens = tokenizer.tokenize(",heLLos plus twoS Fifths ");
-        assertToken(tokens.get(0),  ",",       "",  cat("ignore", "thousand_separator"), cat("point"));
-        assertToken(tokens.get(1),  "heLLos",  " ");
-        assertToken(tokens.get(2),  "plus",    " ", cat("sign", "positive"),             cat("number"));
-        assertToken(tokens.get(3),  "twoS",    " ", cat("number", "digit"),              cat("ordinal"), new Number(2));
-        assertToken(tokens.get(4),  "Fifths",  " ", cat("number", "digit", "ordinal"),   cat("sign"),    new Number(5));
+        assertToken(tokens.get(0),  ",",       "",  0,  cat("ignore", "thousand_separator"), cat("point"));
+        assertToken(tokens.get(1),  "heLLos",  " ", 1);
+        assertToken(tokens.get(2),  "plus",    " ", 8,  cat("sign", "positive"),             cat("number"));
+        assertToken(tokens.get(3),  "twoS",    " ", 13, cat("number", "digit"),              cat("ordinal"), new Number(2));
+        assertToken(tokens.get(4),  "Fifths",  " ", 18, cat("number", "digit", "ordinal"),   cat("sign"),    new Number(5));
     }
 
     @Test
     public void accents() {
         final List<Token> tokens = tokenizer.tokenize("twò ThréèS çòÙplé mInùs");
-        assertToken(tokens.get(0),  "twò",    " ", cat("number", "digit"),               cat("ordinal"),    new Number(2));
-        assertToken(tokens.get(1),  "ThréèS", " ", cat("number", "digit"),               cat("sign"),       new Number(3));
-        assertToken(tokens.get(2),  "çòÙplé", " ", cat("number", "suffix_multiplier"),   cat("multiplier"), new Number(2));
-        assertToken(tokens.get(3),  "mInùs",  "",  cat("sign", "negative"),              cat("number"));
+        assertToken(tokens.get(0),  "twò",    " ", 0,  cat("number", "digit"),               cat("ordinal"),    new Number(2));
+        assertToken(tokens.get(1),  "ThréèS", " ", 4,  cat("number", "digit"),               cat("sign"),       new Number(3));
+        assertToken(tokens.get(2),  "çòÙplé", " ", 11, cat("number", "suffix_multiplier"),   cat("multiplier"), new Number(2));
+        assertToken(tokens.get(3),  "mInùs",  "",  18, cat("sign", "negative"),              cat("number"));
     }
 }
